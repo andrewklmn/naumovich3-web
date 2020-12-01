42bd15b7456e3ec84678f21666b56c873b96e1a4
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-12-01 23:22:58 +0200

    Update: loading states from korovnik

--- api/app/controller/week.php
+++ api/app/controller/week.php
@@ -3,6 +3,7 @@
 
     $state_log_content = load_file_from_korovnik(STATE_LOG_FILENAME);
 
+    
     $array['command'] = "week";
-    $array['records'] = substr($state_log_content,0,50);
+    $array['records'] = explode("\n", $state_log_content);
     
