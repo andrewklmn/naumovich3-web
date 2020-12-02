30157ee08167cd18967e832b7bbade040a230068
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-12-03 00:43:57 +0200

    Add: renter temp section  added

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const API_URL = 'https://litos.kiev.ua/naumovich/api/';
 
+const renterSection = document.querySelector('.renters-temp');
+const fasmebTemp = renterSection.querySelector('.fasmeb-temp');
+const ruslanTemp = renterSection.querySelector('.ruslan-temp');
+
+const stateSection = document.querySelector('.state');
+const pompIcon = stateSection.querySelector('.pomp-icon');
+const pompTemp = stateSection.querySelector('.pomp-temp');
+const heaterIcon = stateSection.querySelector('.heater-icon');
+const heaterTemp = stateSection.querySelector('.heater-temp');
+const outdoorTemp = stateSection.querySelector('.outdoor-temp');
+
 const apiCommands = {
   GET_STATE: '',
   GET_TODAYS_LOG: '?today',
@@ -14,12 +25,12 @@ const heaterModes = {
 }
 
 const state = {
-  heaterParameters: {},
+  heaterParams: {},
 };
 
-const state_indexes = 'config_ram|config_rom|mode|out_s0|litos_s1|mebel_s2|hot_s3|back_s4|out_t0|litos_t1|mebel_t2|hot_t3|back_t4|pomp_OFF|heater_OFF'.split("|");
+const state_indexes = 'configRAM|configROM|currentMode|out_s0|litos_s1|mebel_s2|hot_s3|back_s4|outdoorTemp|fasmebTemp|ruslanTemp|heaterTemp|pompTemp|pompOff|heaterOff'.split("|");
 
-const getHeaterParameters = (state)=> {
+const getheaterParams = (state)=> {
   fetch(API_URL + apiCommands.GET_STATE)
     .then(response => response.json())
     .then((json) => {
@@ -27,20 +38,41 @@ const getHeaterParameters = (state)=> {
       if (stateOfHeater != "Bad request") {
         const state_values = stateOfHeater.split('|');
         state_indexes.forEach((index_name, index)=>{
-          state.heaterParameters[index_name] = state_values[index];
+          state.heaterParams[index_name] = state_values[index];
         });  
-        drawHeaterParameters(state);
+        drawheaterParams(state);
       } else {
         showInfo(stateOfHeater)
       }
     })
     .catch(function() {
-      console.log('Fetch error');
+      showInfo('Network error');
     });
 }
 
-const drawHeaterParameters = ({heaterParameters})=> {
-  console.log(heaterParameters);
+const formatTemp = (heaterParams, parameterName) => {
+  return (heaterParams[parameterName] > 0) 
+            ? '+' + heaterParams[parameterName]:heaterParams[parameterName];
+}
+
+const drawheaterParams = ({heaterParams})=> {
+  fasmebTemp.innerHTML = formatTemp(heaterParams, 'fasmebTemp');
+  ruslanTemp.innerHTML = formatTemp(heaterParams, 'ruslanTemp');
+  pompTemp.innerHTML = formatTemp(heaterParams, 'pompTemp');
+  heaterTemp.innerHTML = formatTemp(heaterParams, 'heaterTemp');
+  outdoorTemp.innerHTML = formatTemp(heaterParams, 'outdoorTemp');
+
+  if (heaterParams.pompOff == '0') {
+    pompIcon.classList.add('on');
+  } else {
+    pompIcon.classList.remove('on');
+  }
+  
+  if (heaterParams.heaterOff == '0') {
+    heaterIcon.classList.add('on');
+  } else {
+    heaterIcon.classList.remove('on');
+  }
 }
 
 const showInfo = (text)=> {
@@ -49,6 +81,6 @@ const showInfo = (text)=> {
 
 document.addEventListener('DOMContentLoaded',() => {
   
-  getHeaterParameters(state);
+  getheaterParams(state);
 
 });
