da35469b2a5f3a535cf945be64cedbdd1cbeb649
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-12-05 17:21:40 +0200

    Add: drawDay D3 function

--- app.js
+++ app.js
@@ -143,6 +143,183 @@ const givenTempChangeHandler = (target, state) => {
   }
 }
 
+const drawDay = (fileContent) => {
+  const OUTDOOR_COLOR = "gray";
+  const ZERO_COLOR = "white";
+  const FASMEB_COLOR = "darkblue";
+  const RUSLAN_COLOR = "darkgreen";
+  const POMP_COLOR = "orange";
+
+  const data = fileContent
+                  .filter(data => data != '' )
+                  .map(data=>{ 
+                    const arr = data.split("|");  
+                    return {
+                      date: arr[0] + "T" + arr[1],
+                      mode: arr[4],
+                      outdoorTemp: arr[10],
+                      ruslanTemp: arr[11],                
+                      fasmebTemp: arr[12],
+                      pompTemp: arr[14],
+                    };
+                });
+
+  const parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
+
+  var margin = {top: 20, right: 20, bottom: 40, left: 50},
+    width = 460 - margin.left - margin.right,
+    height = 215 - margin.top - margin.bottom;
+
+  // set the ranges
+  var x = d3.scaleTime().range([0, width]);
+  var y = d3.scaleLinear().range([height, 0]);
+
+  // define the 1st line
+  var valueline = d3.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.outdoorTemp); });
+  
+  var zeroTemp = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(0); });
+
+  // define the 2nd line
+  var valueline1 = d3.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.ruslanTemp); });
+
+  // define the 3nd line
+  var valueline2 = d3.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.fasmebTemp); });
+
+  // define the 4nd line
+  var valueline3 = d3.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.pompTemp); });    
+
+  // append the svg obgect to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .attr("class", "svg")
+  .append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+  // format the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.outdoorTemp = +d.outdoorTemp;
+      d.ruslanTemp = +d.ruslanTemp;
+      d.fasmebTemp = +d.fasmebTemp;
+      d.mode = +d.mode;
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([
+    d3.min(data, function(d) { return Math.min(
+      d.outdoorTemp, 
+      d.ruslanTemp, 
+      d.fasmebTemp,
+      d.pompTemp
+    ) - 2; }), 
+    d3.max(data, function(d) { return Math.max(
+      d.outdoorTemp, 
+      d.ruslanTemp, 
+      d.fasmebTemp,
+      d.pompTemp
+    ) + 5; })
+  ]);
+
+  // Add the valueline3 path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .style("stroke-dasharray", ("10,3"))
+      .style("stroke", ZERO_COLOR)
+      .attr("d", zeroTemp);
+
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .style("stroke", OUTDOOR_COLOR)
+      .attr("d", valueline);
+
+  // Add the valueline3 path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .style("stroke", POMP_COLOR)
+      .attr("d", valueline3);
+
+  // Add the valueline1 path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .style("stroke", RUSLAN_COLOR)
+      .attr("d", valueline1);
+
+  // Add the valueline2 path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .style("stroke", FASMEB_COLOR)
+      .attr("d", valueline2);
+
+  // Add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%H:%M")))
+      .selectAll("text")	
+        .style("text-anchor", "center")
+        .attr("dx", "0em");
+
+  // Add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left + 5)
+  .attr("x",0 - (height / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("temperature,℃"); 
+
+  svg.append("text")             
+  .attr("transform",
+        "translate(" + (width/2) + " ," + 
+                       (height + margin.top + 15) + ")")
+  .style("text-anchor", "middle")
+  .text("time");
+
+  const startX = width - 100;
+  const startY = 0;
+
+  const legend = [
+    [POMP_COLOR, "Heater"],
+    [RUSLAN_COLOR, "Ruslan"],
+    [FASMEB_COLOR, "Fasmeb"],
+    [OUTDOOR_COLOR, "Outdoor"],
+  ]
+
+  legend.forEach((d, i) => {
+    svg.append("circle").attr("cx",startX).attr("cy", startY + i*15).attr("r", 4).style("fill", d[0])
+    svg.append("text")
+       .attr("x", startX + 10)
+       .attr("y", startY + i*15)
+       .text(d[1])
+       .style("font-size", "12px")
+       .attr("alignment-baseline","middle")
+  });
+  
+};
+
 const postNewTemp = (temp, state) => {
   loaderImage.classList.remove('hidden');
   
@@ -162,8 +339,44 @@ const postNewTemp = (temp, state) => {
     });
 };
 
+const getDayLog = (state)=> {
+  /*
+  showInfo('<span style="color:white;">ping</span>');
+  loaderImage.classList.remove('hidden');
+
+  fetch(API_URL + apiCommands.GET_STATE)
+    .then(response => response.json())
+    .then((json) => {
+      const {stateOfHeater, givenTemp} = json; 
+      if (stateOfHeater!=null 
+            && stateOfHeater != "Bad request"
+            && stateOfHeater != "T") {
+        const state_values = stateOfHeater.split('|');
+        state_indexes.forEach((index_name, index)=>{
+          state.heaterParams[index_name] = state_values[index];
+        });  
+        drawheaterParams(state);
+        if (!givenTempSetter.classList.contains('loading')) setGivenTemp(givenTemp);
+        showInfo('pong');
+        clearInfo();
+        setTimeout(()=>getHeaterParams(state), refreshingPeriodInSec*1000 );
+      } else {
+        showInfo(stateOfHeater);
+        setTimeout(()=>getHeaterParams(state), refreshingAfterError*1000 );
+      }
+      loaderImage.classList.add('hidden');
+    })
+    .catch(function() {
+      showInfo('<span style="color:red;">Network error!</span>');
+      loaderImage.classList.add('hidden');
+      setTimeout(()=>getHeaterParams(state), refreshingAfterError*1000 );      
+    });
+  */
+}
+
 document.addEventListener('DOMContentLoaded',() => {
   drawTempSetter(MIN_GIVEN_TEMP, MAX_GIVEN_TEMP, GIVEN_TEMP_STEP);
   getHeaterParams(state);
   givenTempSetter.addEventListener('change',({target}) => givenTempChangeHandler(target, state));
+
 });
