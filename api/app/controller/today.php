a002e2fe22fd9b6cd8ca3a4049e8186449231294
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-12-05 21:16:29 +0200

    Update: last 7 days selector in backend controller

--- api/app/controller/today.php
+++ api/app/controller/today.php
@@ -12,8 +12,6 @@
     
     $state_lines = explode("\n", load_file_from_korovnik(STATE_LOG_FILENAME));
     
-    // 2020-12-04|02:11:45|
-    
     foreach ($state_lines as $line) {
         if ((substr($line,0,10) == $yesterday[0] AND substr($line,11,8) >= $today[1])
                 OR (substr($line,0,10) == $today[0] AND substr($line,11,8) <= $today[1])) {
--- api/app/controller/week.php
+++ api/app/controller/week.php
@@ -2,6 +2,21 @@
     
     $array['command'] = "week";
     
-    $state_log_content = load_file_from_korovnik(STATE_LOG_FILENAME);
-    $array['records'] = explode("\n", $state_log_content);
+    $date = new DateTime(date());
+    $array['todayDate'] = $date->format('Y-m-d H:i:s');;
+    $date->modify('-7 day');
+    $array['startDate'] = $date->format('Y-m-d H:i:s');;
+    
+    $today = explode(" ", $array['todayDate']);
+    $start_day = explode(" ", $array['startDate']);
+    
+    $state_lines = explode("\n", load_file_from_korovnik(STATE_LOG_FILENAME));
+    
+    foreach ($state_lines as $line) {
+        if ( (substr($line,0,10) == $start_day[0] AND substr($line,11,8) >= $today[1])
+                OR (substr($line,0,10) > $start_day[0] AND substr($line,0,10) < $today[0])
+                OR (substr($line,0,10) == $today[0] AND substr($line,11,8) <= $today[1])) {
+            $array['records'][] = $line;         
+        }
+    }
     
