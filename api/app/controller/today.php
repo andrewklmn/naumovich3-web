2c776540c88818f635fc690c0348360002d94502
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-12-06 23:24:31 +0200

    Update: today data function on backend

--- api/app/controller/today.php
+++ api/app/controller/today.php
@@ -3,10 +3,10 @@
     $array['command'] = "today";
     
     $date = new DateTime(date());
-    $date->modify('+1 hour');
     $array['todayDate'] = $date->format('Y-m-d H:i:s');;
-    $date->modify('-25 hour');
+    $date->modify('-24 hour');
     $array['yesterdayDate'] = $date->format('Y-m-d H:i:s');;
+    
     
     $today = explode(" ", $array['todayDate']);
     $yesterday = explode(" ", $array['yesterdayDate']);
@@ -14,9 +14,8 @@
     $state_lines = explode("\n", load_file_from_korovnik(STATE_LOG_FILENAME));
         
     foreach ($state_lines as $line) {
-        if ( (substr($line,0,10) == $yesterday[0] AND substr($line,11,8) >= $today[1])
-                OR (substr($line,0,10) > $yesterday[0] AND substr($line,0,10) < $today[0])
-                OR (substr($line,0,10) == $today[0] AND substr($line,11,8) <= $today[1])) {
+        if ((substr($line,0,10) == $yesterday[0] AND substr($line,11,8) >= $today[1])
+             OR (substr($line,0,10) == $today[0] AND substr($line,11,8) <= $today[1])) {
             $array['records'][] = $line;         
         }
     }
